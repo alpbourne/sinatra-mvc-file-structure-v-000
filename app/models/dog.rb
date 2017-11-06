1bf19f8ca1346919b1fd14a8a6c69dfacc3b4145
alpbourne <alpbourne@gmail.com>
2017-11-06 19:44:21 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,4 +1,4 @@
-class Dog 
+class Dog
   attr_accessor :name, :breed, :age
 
   @dogs = []
@@ -7,5 +7,11 @@ class Dog
     @name = name
     @breed = breed
     @age = age
+    @dogs << self
   end
-  
+
+  def self.all 
+    @dogs
+  end
+
+end
